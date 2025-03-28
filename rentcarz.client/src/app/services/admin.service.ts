d2550f65b218b3031cf03a3e41458e0c7c299d80
greydontmatter <107159312+greydontmatter@users.noreply.github.com>
2025-03-27 22:36:27 -0400

    added update and delete operations for adminserviceand controller in backend and service in frontend, need to complete frontend implementation on car listings when admin is logged in

--- rentcarz.client/src/app/services/admin.service.ts
+++ rentcarz.client/src/app/services/admin.service.ts
@@ -8,9 +8,21 @@ import { Observable } from 'rxjs';
 export class AdminService {
   private apiUrl = 'http://localhost:5240/api/admins'; // Update with your actual API base URL
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   addCar(adminId: number, carData: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/addCar?adminId=${adminId}`, carData);
   }
+
+  // Update existing car details
+  updateCar(carId: number, updatedCarData: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/editCar/${carId}`, updatedCarData);
+  }
+
+  // Delete car listing
+  deleteCar(carId: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/deleteCar/${carId}`);
+  }
+
+
 }
